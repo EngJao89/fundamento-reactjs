4c20c1e609942283cf912a72bb4d173a3b0672d6
EngJao89 <eng_joao@outlook.com>
2023-08-23 19:50:03 -0300

    feat(if03-post): implementa card de post no sistema

---
+++ src/components/Post.jsx
@@ -0,0 +1,38 @@
+import styles from './Post.module.css';
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+      <div className={styles.author}>
+          <div className={styles.authorInfo}>
+            <strong>João Ricardo</strong>
+            <span>Software Developer</span>
+          </div>
+        </div>
+
+        <time title="11 de Julho ás 17:00" dateTime="11-07-22 08:13:30">
+          Publicado a 1 mês
+        </time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+
+        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+
+        <p>
+          <a>
+          👉 joao.software/mobiledev
+          </a>
+        </p>
+
+        <p>
+          <a>
+            novoprojeto #nlw #rocketseat
+          </a>
+        </p>
+      </div>
+    </article>
+  )
+}
