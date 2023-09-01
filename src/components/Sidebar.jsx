41ba82980a7cb4940d44c24f9fe084803501588e
EngJao89 <eng_joao@outlook.com>
2023-09-01 09:24:17 -0300

    fix(if14-types-fix): corrige sidebar e tipagens

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,7 @@
 import { PencilLine } from 'phosphor-react'
+import { Avatar } from './Avatar';
 
 import styles from './Sidebar.module.css';
-import { Avatar } from './Avatar';
 
 export function Sidebar() {
   return (
@@ -12,10 +12,10 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
-        <Avatar src="https://github.com/EngJao89.png"/>
+        <Avatar src="https://github.com/maykbrito.png" />
 
-        <strong>João Ricardo</strong>
-        <span>Software Developer</span>
+        <strong>Diego Fernandes</strong>
+        <span>Web Developer</span>
       </div>
 
       <footer>
