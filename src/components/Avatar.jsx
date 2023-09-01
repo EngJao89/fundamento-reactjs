58c24788fde32e79dff1350288fa65d0109e8a6c
EngJao89 <eng_joao@outlook.com>
2023-09-01 08:44:36 -0300

    feat(if11-types-avatar): implementa tipagem de avatar de usuário

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,5 +1,15 @@
-export function Avatar(props){
-  return(
-    <img className={StyleSheet.avatar} src={props.src} />
-  )
+import { ImgHTMLAttributes } from "react";
+import styles from './Avatar.module.css'
+
+interface AvatarProps extends ImgHTMLAttributes<HTMLImageElement> {
+  hasBorder?: boolean;
+}
+
+export function Avatar({ hasBorder = true, ...props}: AvatarProps) {
+  return (
+    <img
+      className={hasBorder ? styles.avatarWithBorder : styles.avatar}
+      {...props}
+    />
+  );
 }
