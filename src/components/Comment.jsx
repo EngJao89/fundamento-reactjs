d03b92734c4379fbe1f731b207c475e9c985692c
EngJao89 <eng_joao@outlook.com>
2023-09-01 08:50:58 -0300

    feat(if12-types-comment): implementa tipagem de comentários

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,13 +1,34 @@
+import { useState } from "react";
 import styles from './Comment.module.css';
 
 import { ThumbsUp, Trash } from 'phosphor-react';
+import { Avatar } from './Avatar';
 
+interface CommentProps {
+  content: string;
+  onDeleteComment: (comment: string) => void;
+}
 
+export function Comment({ content, onDeleteComment }: CommentProps) {
+  const [likeCount, setLikeCount] = useState(0);
 
-export function Comment() {
+  function handleDeleteComment() {
+    onDeleteComment(content);
+  }
+
+  function handleLikeComment() {
+    setLikeCount((state) => {
+      return state + 1
+    });
+  }
 
   return (
     <div className={styles.comment}>
+      <Avatar
+        hasBorder={false}
+        src="https://github.com/diego3g.png"
+        alt=""
+      />
 
       <div className={styles.commentBox}>
         <div className={styles.commentContent}>
@@ -17,18 +38,18 @@ export function Comment() {
               <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:00">Cerca de 1h atrás</time>
             </div>
 
-            <button title="Deletar comentário">
+            <button onClick={handleDeleteComment} title="Deletar comentário">
               <Trash size={24} />
             </button>
           </header>
 
-          <p>Parabéns!</p>
+          <p>{content}</p>
         </div>
 
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp />
-            Aplaudir <span></span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
